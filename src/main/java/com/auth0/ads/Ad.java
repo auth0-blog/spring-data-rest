b063aa06665574f8e5d924667fbe670356917ed7
Bruno Krebs <bruno.krebs@auth0.com>
2019-02-19 18:58:08 -0300

    adding data validation;

--- src/main/java/com/auth0/ads/Ad.java
+++ src/main/java/com/auth0/ads/Ad.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Entity
@@ -17,13 +20,21 @@ public class Ad {
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
 
+    @NotNull(message = "Who is the owner of this ad?")
     public String owner;
+
+    @NotNull(message = "Please, inform a title for your ad.")
+    @Size(min = 5, max = 140, message = "Titles must have between {min} and {max} characters.")
     public String title;
+
+    @NotNull(message = "Please, inform a description for your ad.")
+    @Size(min = 5, max = 500, message = "Titles must have between {min} and {max} characters.")
     public String description;
+
+    @Min(message = "Price cannot be negative", value = 0)
     public BigDecimal price;
 
-    protected Ad() {
-    }
+    protected Ad() {}
 
     public Ad(String owner, String title, String description, BigDecimal price) {
         this.owner = owner;
---
+++ src/main/java/com/auth0/ads/RestExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.auth0.ads;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.RollbackException;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(TransactionSystemException.class)
+    protected ResponseEntity<List<String>> handleTransactionException(TransactionSystemException ex) throws Throwable {
+        Throwable cause = ex.getCause();
+        if (!(cause instanceof RollbackException)) throw cause;
+        if (!(cause.getCause() instanceof ConstraintViolationException)) throw cause.getCause();
+        ConstraintViolationException validationException = (ConstraintViolationException) cause.getCause();
+        List<String> messages = validationException.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage).collect(Collectors.toList());
+        return new ResponseEntity<>(messages, HttpStatus.BAD_REQUEST);
+    }
+}
